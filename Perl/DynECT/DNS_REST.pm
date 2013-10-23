eb6114e8f75b0b9a5021be1137a331efcfbbd990
Ryan D. Brickett <rbrickett@dyn.com>
2013-10-22 21:15:28 -0400

    Initial commit of QPS for past 28 days

--- Perl/DynECT/DNS_REST.pm
+++ Perl/DynECT/DNS_REST.pm
@@ -1,27 +1,44 @@
 #!/usr/bin/env perl
 
+$VERSION = "1.01";
+$VERSION = eval $VERSION;
+
 package DynECT::DNS_REST;
 
 use strict;
 use warnings;
 use LWP::UserAgent;
 use JSON;
+use Data::Dumper;
+
+#TODO: Write documentation with POD
+#TODO: Add keepalive
 
 #Constructor
 sub new {	
 	#reference to self if first argument passed in
 	my $classid = shift;
+	my %con_args = @_;
 	
 	my $self = {
 		#LWP User agent instance
 		lwp => '',		
-		apikey => '',
+		apitoken => undef,
+		apiver => undef,
 		#Current status meesage
 		message => '',
 		#Reference to a hash for JSON decodes of most recent result
 		resultref => '',
 	};
 
+	$$self{'apiver'} = $con_args{ 'version' } if ( exists $con_args{ 'version' } );
+
+	$$self{'lwp'} = LWP::UserAgent->new;
+	#diable redirect following as that is a special case with DynECT
+	$$self{'lwp'}->max_redirect( '0' );
+	#reduce timeout from 180 seconds to 20
+	$$self{'lwp'}->timeout( '20' );
+	
 	bless $self, $classid;
 
 	return $self;
@@ -31,100 +48,92 @@ sub new {
 sub login {
 	#get reference to self
 	#get params from call
-	my ( $classid, $custn, $usern, $pass) = @_;
+	my ($classid, $custn, $usern, $pass) = @_;
 
 	#API login
-	my $session_uri = 'https://api2.dynect.net/REST/Session';
 	my %api_param = (
 		'customer_name' => $custn,
 		'user_name' => $usern,
 		'password' => $pass,
 	);
 
-	my $api_request = HTTP::Request->new('POST','https://api2.dynect.net/REST/Session');
-	$api_request->header ( 'Content-Type' => 'application/json' );
-	$api_request->content( to_json( \%api_param ) );
-
-	$classid->{'lwp'} = LWP::UserAgent->new;
-	my $api_result = $classid->{'lwp'}->request( $api_request );
-
-	#check if call succeeded
-	my $res = $classid->check_res( $api_result );
+	my $res = $classid->request( 'OVERRIDESESSION', 'POST', \%api_param);
 	if ( $res ) {
-		#Grab API key
-		$classid->{'apikey'} = $classid->{'resultref'}->{'data'}->{'token'};
-		$classid->{'message'} = "Login successful";
-		return 1;
+		$$classid{'apitoken'} = $$classid{'resultref'}{'data'}{'token'};
+		$$classid{'message'} = 'Session successfully created';
 	}
-	else {
-		return $res;
-	}
+	return $res;
 }
 
 sub logout {
 	#get self id
 	my $classid = shift;
-
+	#TODO: Set message if API token not set
 	#existance of the API key means we are logged in
-	if ( $classid->{'apikey'} ) {
+	if ( $$classid{'apitoken'} ) {
 		#Logout of the API, to be nice
-		my $api_request = HTTP::Request->new('DELETE','https://api2.dynect.net/REST/Session');
-		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apikey'} );
-		my $api_result = $classid->{'lwp'}->request( $api_request );
+		my $api_request = HTTP::Request->new('DELETE','https://api.dynect.net/REST/Session');
+		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
+		my $api_result = $$classid{'lwp'}->request( $api_request );
 		my $res =  $classid->check_res( $api_result );
 		if ( $res ) {
-			undef $classid->{'apikey'};
-			$classid->{'message'} = "Logout successful";
-			return $res;
+			undef $$classid{'apitoken'};
+			$$classid{'message'} = "Logout successful";
 		}
-		else {
-			return $res;
-		}
+		
+		return $res;
 	}
 }
 
 sub request {
-	my ($classid, $uri, $method) = @_;
-	my $paramref = '';
-	if (exists $_[3]) {
-		$paramref = $_[3]; 
+	my ($classid, $uri, $method, $paramref) = @_;
+	if (defined $paramref) {
 		#weak check for correct paramater type
 		unless ( ref($paramref) eq 'HASH' ) {
-			$classid->{'message'} = "Invalid paramater type.  Please utilize a hash reference";
+			$$classid{'message'} = "Invalid paramater type.  Please utilize a hash reference";
 			return 0;
 		}
 	}
-	#TODO: Set this to detect start of string
+
 	if ( $uri =~ /\/REST\/Session/ ) {
-		$classid->{'message'} = "Please use the ->login or ->logout for managing sessions";
+		$$classid{'message'} = "Please use the ->login, ->keepalive, or ->logout for managing sessions";
 		return 0;
-		}
+	}
+
+	#catch internal use of session URI
+	$uri = '/REST/Session/' if ( $uri eq 'OVERRIDESESSION' ); 
 
 	#weak check for valid URI
-	if ( !($uri =~ /\/REST\//) || ( uc($uri) =~ /HTTPS/ )  ) {
-		$classid->{'message'} = "Invalid REST URI.  Correctly formatted URIs start with '/REST/";
+	unless ( $uri =~ /^\/REST\// ) {
+		$$classid{'message'} = "Invalid REST URI.  Correctly formatted URIs start with '/REST/";
 		return 0;
-
 	}
 
-	my $api_request = HTTP::Request->new(uc($method), "https://api2.dynect.net$uri");
-	$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apikey'} );
-	if ($paramref) {
+	#Check for valid method type
+	$method = uc( $method );
+	unless ( $method eq 'GET' || $method eq 'POST' || $method eq 'PUT' || $method eq 'DELETE' ) {
+		$$classid{ 'message' } = 'Invalid method type.  Please use GET, PUT, POST, or DELETE.';
+		return 0;
+	}
+
+	my $api_request = HTTP::Request->new( $method , "https://api.dynect.net$uri");
+	$api_request->header ( 'Content-Type' => 'application/json' );
+	$api_request->header ( 'Auth-Token' => $$classid{'apitoken'} ) if ( defined $$classid{'apitoken'} );
+	$api_request->header ( 'Version' => $$classid{'apiver'} ) if ( defined $$classid{'apiver'} );
+
+	if (defined $paramref) {
 		$api_request->content( to_json( $paramref ) );
 	}
-	else {
-		$api_request->content();
-	}
-	my $api_result = $classid->{'lwp'}->request( $api_request );
+
+	my $api_result = $$classid{'lwp'}->request( $api_request );
+
 	#check if call succeeded
 	my $res =  $classid->check_res( $api_result );
-	if ( $res ) {
-		$classid->{'message'} = "Request ( $uri, $method) successful";
-		return $res;
-	}
-	else {
-		return $res;
-	}
+
+	# If $res, set the message
+	$$classid{'message'} = "Request ( $uri, $method) successful" if $res;
+	
+	return $res;
 }
 
 
@@ -134,72 +143,78 @@ sub check_res {
 	
 	#Fail out if there is no content in the response
 	unless ($api_result->content) { 
-		$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+		$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 		return 0;
 	}
 
 	#on initial redirect the result->code is the URI to the Job ID
 	#Calling the /REST/Job will return JSON in the content of status 
 	if ($api_result->code == 307) {
-		my $api_request = HTTP::Request->new('GET', "https://api2.dynect.net" . $api_result->content);
-		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apikey'} );
-		$api_result = $classid->{'lwp'}->request( $api_request );
+		sleep 2;
+		my $api_request = HTTP::Request->new('GET', "https://api.dynect.net" . $api_result->content);
+		$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
+		$api_result = $$classid{'lwp'}->request( $api_request );
 		unless ( $api_result->content ) { 
-			$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+			$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 			return 0;
 		}
 	}
-	
+
 	#now safe to decode JSON
-	$classid->{'resultref'} = decode_json ( $api_result->content );
+	$$classid{'resultref'} = decode_json ( $api_result->content );
 
 	#loop until the job id comes back as success or program dies
-	while ( $classid->{'resultref'}->{'status'} ne 'success' ) {	
-		if ($classid->{'resultref'}->{'status'} ne 'incomplete') {
+	while ( $$classid{'resultref'}{'status'} ne 'success' ) {
+		if ( $$classid{'resultref'}{'status'} ne 'incomplete' ) {
 			#api stauts != sucess || incomplete would indicate an API failure
-			foreach my $msgref ( @{$classid->{'resultref'}->{'msgs'}} ) {
-				$classid->{'message'} = "API Error:\n";
-				$classid->{'message'} .= "\tInfo: $msgref->{'INFO'}\n" if $msgref->{'INFO'};
-				$classid->{'message'} .= "\tLevel: $msgref->{'LVL'}\n" if $msgref->{'LVL'};
-				$classid->{'message'} .= "\tError Code: $msgref->{'ERR_CD'}\n" if $msgref->{'ERR_CD'};
-				$classid->{'message'} .= "\tSource: $msgref->{'SOURCE'}\n" if $msgref->{'SOURCE'};
+			foreach my $msgref ( @{$$classid{'resultref'}->{'msgs'}} ) {
+				$$classid{'message'} = "API Error:\n";
+				$$classid{'message'} .= "\tInfo: $msgref->{'INFO'}\n" if $msgref->{'INFO'};
+				$$classid{'message'} .= "\tLevel: $msgref->{'LVL'}\n" if $msgref->{'LVL'};
+				$$classid{'message'} .= "\tError Code: $msgref->{'ERR_CD'}\n" if $msgref->{'ERR_CD'};
+				$$classid{'message'} .= "\tSource: $msgref->{'SOURCE'}\n" if $msgref->{'SOURCE'};
 			};
 			return 0;
 		}
 		else {
-			#status incomplete, wait 5 seconds and check again
-			sleep(2);
-			my $job_uri = "https://api2.dynect.net/REST/Job/$classid->{'resultref'}->{'job_id'}/";
+			#status incomplete, wait 2 seconds and check again
+			sleep 2;
+			my $job_uri = "https://api.dynect.net/REST/Job/$$classid{'resultref'}{'job_id'}/";
 			my $api_request = HTTP::Request->new('GET',$job_uri);
-			$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $classid->{'apikey'} );
-			my $api_result = $classid->{'lwp'}->request( $api_request );
+			$api_request->header ( 'Content-Type' => 'application/json', 'Auth-Token' => $$classid{'apitoken'} );
+			my $api_result = $$classid{'lwp'}->request( $api_request );
 			unless ( $api_result->content ) { 
-				$classid->{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
+				$$classid{'message'} = "Unable to connect to API.\n Status message -\n\t" . $api_result->status_line;
 				return 0;
 			}
-			$classid->{'resultref'} = decode_json( $api_result->content );
+			$$classid{'resultref'} = decode_json( $api_result->content );
 		}
 	}
+	
 	return 1;
+}
+
+sub version {
+	my $classid = shift;
+	my $ver = shift;
+	$$classid{'apiver'} = $ver if ( defined $ver );
+	return $$classid{'apiver'};
 }
 
 
 sub message {
 	my $classid = shift; 
-	return $classid->{'message'};
+	return $$classid{'message'};
 }
 
 sub result {
 	my $classid = shift;
-	return $classid->{'resultref'};
+	return $$classid{'resultref'};
 }
 
 sub DESTROY {
-	#get self id
-	my $classid = shift;
 	#call logout on destroy
-	$classid->logout();
+	$_[0]->logout();
 }
-
 
 1;
--- Perl/Usage_Details.pl
+++ Perl/Usage_Details.pl
@@ -1,110 +1,17 @@
 #!/usr/bin/env perl
 
-#This script will print a QPS report for all zones in an account, all hostnames in a zone, or record types in a hostname. 
-#Optionally, you can display the header information (off by default). You can send the QPS report to a CSV file.
-#You can set your own breakdown using zones, hosts, or rrecs. The default values are in the help menu.
-
-# Options:
-# -h --help		Show the help message and exit
-# -a --all		Outputs QPS for all zones
-# -z --zone		Return the QPS by hosts
-# -n --node		Return the record QPS for a specific node (hostname)
-# -s --start		Start Date for QPS(ie: 07-01-2013) Start time begins on 00:00:01
-# -e --end		End Date for QPS(ie: 07-15-2013) End time begins on 23:59:59
-# -f --file		File to output data to in csv format
-# -t --title		Prints the header information (Default is off)
-# -b --breakdown	Set a custom breakdown. Defaults: -a: zones -z: hosts -n: rrecs
-
-# Example Usage
-# perl Usage_Details.pl -z [example.com] -s [2013-07-01] -e [2013-07-15]
-# 	Will print out the QPS for each node in the zone, example.com
-
-# perl Usage_Details.pl -z [node.example.com] -s [2013-07-01] -e [2013-07-15] -f [filename.csv]
-# 	Will write the file to filename.csv with the QPS for the node in the zone, node.example.com
-
-
 use warnings;
 use strict;
 use Config::Simple;
 use Getopt::Long;
 use Time::Local;
 use Text::CSV_XS;
+use Data::Dumper;
 
 #Import DynECT handler
 use FindBin;
 use lib "$FindBin::Bin/DynECT";  # use the parent directory
 require DynECT::DNS_REST;
-
-my $opt_zone="";
-my $opt_node="";
-my $opt_file="";
-my $opt_start="";
-my $opt_end="";
-my $opt_breakdown="";
-my $opt_help;
-my $opt_all;
-my $opt_title;
-my $fh;
-my $csv = Text::CSV_XS->new ( { binary => 1, eol => "\n" } ) or die "Cannot use CSV: ".Text::CSV->error_diag ();
-
-GetOptions(
-	'help' => \$opt_help,
-	'file=s' => \$opt_file,
-	'all' => \$opt_all,
-	'title' => \$opt_title,
-	'breakdown=s' =>\$opt_breakdown,
-	'zone=s' =>\$opt_zone,
-	'node=s' =>\$opt_node,
-	'start=s' =>\$opt_start,
-	'end=s' => \$opt_end,
-);
-
-#Printing help menu
-if ($opt_help) {
-	print "\nOptions:\n";
-	print "-h --help\t Show the help message and exit\n";
-	print "-a --all\t Outputs QPS for all zones\n";
-	print "-z --zone\t Return the QPS by hosts\n";
-	print "-n --node\t Return the record QPS for a specific node (hostname)\n";
-	print "-s --start\t Start Date for QPS(ie: 07-01-2013) Start time begins on 00:00:01\n";
-	print "-e --end\t End Date for QPS(ie: 07-15-2013) End time begins on 23:59:59\n";
-	print "-f --file\t File to output data to in csv format\n";
-	print "-t --title\t Prints the header information (Default is off)\n";
-	print "-b --breakdown\t Set a custom breakdown. Defaults: -a: zones -z: hosts -n: rrecs\n";
-	print "\nUsage Example:\n";
-	print "perl Usage_Details.pl -z [example.com] -s [2013-07-01] -e [2013-07-15]\n\tWill print out the QPS for each node in the zone, example.com\n";
-	print "perl Usage_Details.pl -z [node.example.com] -s [2013-07-01] -e [2013-07-15] -f [filename.csv]\n\tWill write the file to filename.csv with the QPS for the node in the zone, node.example.com\n";
-	exit;
-}
-
-#Zone & node are required
-elsif ($opt_zone eq "" && $opt_node eq "" && !$opt_all)
-{
-	print "Need to use \"-a\" \"-z [example.com]\" or \"-n [node.example.com]\".\n";
-	exit;
-}
-# Setup the csv file for writing
-elsif($opt_file ne "")
-{
-	# Setting up new csv file
-	open $fh, ">", $opt_file  or die "new.csv: $!";
-}
-
-# Ensure both -s and -e are set
-if ($opt_start && $opt_end )
-{
-	#Set start and end timestamps to proper format
-	#Set month to m-1 because timelocal month starts at 0
-	my ($y, $m, $d) = split '-', $opt_start;
-	$opt_start = timegm(1,0,0,$d,$m-1,$y);
-	($y, $m, $d) = split '-', $opt_end;
-	$opt_end = timegm(59,59,23,$d,$m-1,$y);
-}
-else
-{
-	print "Need to use \"-s [2013-07-01]\" and \"-e [2013-07-15]\"\n";
-	exit;
-}
 
 
 #Create config reader
@@ -129,71 +36,87 @@ my $apipw = $configopt{'pw'} or do {
 	exit;
 };
 
+my $csv = Text::CSV_XS->new ( { binary => 1 } ) or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
 #API login
 my $dynect = DynECT::DNS_REST->new;
 $dynect->login( $apicn, $apiun, $apipw) or
 	die $dynect->message;
 
-my %api_param;
-my $api_decode;
-#Set the parameters if either fqdn or zone is set.
-if($opt_node ne "")	{
-	$opt_breakdown = 'rrecs' unless($opt_breakdown ne ""); 
-	%api_param = (start_ts => $opt_start, end_ts => $opt_end, breakdown => $opt_breakdown, hosts => $opt_node )
-}
-elsif($opt_zone ne "") {
-	$opt_breakdown = 'hosts' unless($opt_breakdown ne ""); 
-	%api_param = (start_ts => $opt_start, end_ts => $opt_end, breakdown => $opt_breakdown, zones => $opt_zone )
-}
-else {
-	$opt_breakdown = 'zones' unless($opt_breakdown ne ""); 
-	%api_param = (start_ts => $opt_start, end_ts => $opt_end, breakdown => $opt_breakdown )
-}
+my ($day, $month, $year) = ( gmtime() )[3 .. 5];
+my $endtime = timegm( 0, 0, 0, $day, $month, $year );
+my $start_ts = $endtime - 2419200; #604800;
+my %api_param = ( breakdown => 'zones');
+my $rep_day = 0;
+my %week1query;
+my %week2query;
+my %week3query;
+my %week4query;
+my %monthquery;
 
-$dynect->request( '/REST/QPSReport', 'POST',  \%api_param)
-	or die $dynect->message;
-$api_decode = $dynect->result; 
+while ( $start_ts < $endtime ) {
+	$api_param{ 'start_ts' } = $start_ts;
+	$api_param{ 'end_ts' } = $start_ts + 86399;		#just shy of a full day
+	$dynect->request ( '/REST/QPSReport', 'POST', \%api_param);
+	print Dumper $dynect->result;
 
-#Store the returned csv string
-my $csv_string = ( $api_decode->{'data'}->{'csv'});
-
-#Read in the csv from the response
-my %hash;
-my $linenum = 0;
-
-#Read in each line one at a time.
-my @lines = split /\n/, $csv_string;
-foreach my $line (@lines){
-	#Set each value in the csv to timestamp, hostname, queries
-	$csv->parse ($line);
-	my @columns = $csv->fields ();
-	my($t, $h, $q) = @columns;
-	
-	#If its the first line, save it
-	if ($linenum == 0){
-		print "$q\t\t$h\n" unless($opt_file ne "" || !$opt_title);
-		$csv->print ($fh, [ $q, $h] ) unless($opt_file eq "" || !$opt_title);
+	my @lines = split /\n/, $dynect->result->{'data'}{'csv'};
+	foreach my $line ( @lines ) {
+		$csv->parse( $line );
+		my @fields = $csv->fields;
+		next if $fields[0] eq 'Timestamp';
+		if ( $rep_day < 7 ) {
+			push @{$week1query{$fields[1]}}, $fields[2];
+		}
+		elsif ( $rep_day < 14 ) {
+			push @{$week2query{$fields[1]}}, $fields[2];
+		}
+		elsif ( $rep_day < 21 ) {
+			push @{$week3query{$fields[1]}}, $fields[2];
+		}
+		elsif ( $rep_day < 28 ) {
+			push @{$week4query{$fields[1]}}, $fields[2];
+		}
+		push @{$monthquery{$fields[1]}}, $fields[2];
 	}
-	#Else if the hash exists, add the queries up
-	else{
-		$hash{$h} += $q;
-	}
-	$linenum ++;
-}
-#Goes through the hash printing the queries to the string
-foreach my $hostname ( sort keys %hash ){
-	print "$hash{$hostname}\t\t$hostname\n" unless($opt_file ne "");
-	$csv->print ($fh, [ $hash{$hostname}, $hostname] ) unless($opt_file eq "");
+
+
+	$start_ts  += 86400;
+	$rep_day++;
+	sleep 1;
 }
 
+foreach my $zone ( keys %monthquery ){
+	my @csvprint = ( $zone, 0, 0, 0, 0 );
+	$csvprint[1] = ninefive( $week1query{ $zone } ) if ( exists $week1query{ $zone } );
+	$csvprint[2] = ninefive( $week2query{ $zone } ) if ( exists $week2query{ $zone } );
+	$csvprint[3] = ninefive( $week3query{ $zone } ) if ( exists $week3query{ $zone } );
+	$csvprint[4] = ninefive( $week4query{ $zone } ) if ( exists $week4query{ $zone } );
+	$csvprint[5] = ninefive( $monthquery{ $zone } );
+	print Dumper \@csvprint;
+}
 
 # Close csv file
-if($opt_file ne "")
-{
-close $fh or die "$!";
-print "CSV file: $opt_file written sucessfully.\n";
-}
+#if($opt_file ne "")
+#{
+#close $fh or die "$!";
+#print "CSV file: $opt_file written sucessfully.\n";
+#}
 
 #api logout
 $dynect->logout;
 
+
+
+sub ninefive {
+	my $arrref = shift @_;
+	my @queries = sort {$a <=> $b} @$arrref;
+	#This round towards zeo.  Normally for 95% you would round up but the array index 
+	#starts at 0 effectively giving a +1 to the calculaiton
+	my $ninefive = int( scalar( @queries )  * 0.95 ) ;
+
+	#Truncate to 3 signifigant digits
+	my $qps = int( ($queries[$ninefive] / 300 ) * 1000 ) / 1000;
+
+	return $qps;
+}
